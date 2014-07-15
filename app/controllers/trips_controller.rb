1550e76aeecce2a95c26697ad55654b7d58584e6
Ben Burke <benjamin.burke@yale.edu>
2014-07-15 16:17:55 -0400

    working on form

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,5 +2,21 @@ class TripsController < ApplicationController
 
 	def new
 		@user = User.find_by(netid: session[:cas_user])
+		@trip = @user.trips.new
 	end 
+
+	def update
+		if @user.update_attributes(user_params)
+			flash[:success] = "Account Created"
+  		redirect_to root_url
+  	end 
+	end 
+
+
+	private
+		def user_params
+			params.require(:user).permit(:name, :email)
+   	end 
+
+
 end
