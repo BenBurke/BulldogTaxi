99e3566f3c56ac5c99f55e3f9730ec24637f3d66
Ben Burke <benjamin.burke@yale.edu>
2014-07-16 16:33:30 -0400

    working on date

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   end 
   def update
   	@user = User.find_by(netid: session[:cas_user])
+    params[:user][:phone_number] = phoneNumber(params[:user][:phone_number])
   	if @user.update_attributes(user_params)
   		flash[:success] = "Account Created"
   		redirect_to controller: 'trips', action: 'new'
@@ -15,8 +16,15 @@ class UsersController < ApplicationController
   	@user = User.find_by(netid: session[:cas_user])
   end 
 
+  def phoneNumber(input)
+    if input.gsub(/\D/, "").match(/^1?(\d{3})(\d{3})(\d{4})/)
+      result = [$1, $2, $3].join("")
+      return result.to_i
+    end
+  end 
+
   private
    def user_params
-   	params.require(:user).permit(:name, :email)
+   	params.require(:user).permit(:name, :email, :phone_number)
    end
 end
