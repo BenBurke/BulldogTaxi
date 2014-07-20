8464fc6f7ea4fcd9beaded54e43191a3cac93c89
Ben Burke <benjamin.burke@yale.edu>
2014-07-20 15:19:49 -0400

    working on founders page etc

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,12 +7,12 @@ class UsersController < ApplicationController
   def update
   	@user = User.find_by(netid: session[:cas_user])
     params[:user][:phone_number] = phoneNumber(params[:user][:phone_number])
-    if params[:page] == "1" # this directs the redirect based upon hidden field returns for edit.html.erb & (user)new.html.erb
+    if params[:page] == "form" # this directs the redirect based upon hidden field returns for edit.html.erb & (user)new.html.erb
     	if @user.update_attributes(user_params)
     		flash[:success] = "Welcome " + @user.name.split(" ")[0] + "!"
     		redirect_to controller: 'trips', action: 'new'
     	end
-    elsif params[:page] == "0"
+    elsif params[:page] == "edit"
       if @user.update_attributes(user_params)
         flash[:success] = "Profile Updated!"
         redirect_to @user
@@ -21,6 +21,7 @@ class UsersController < ApplicationController
   end 
   def show
   	@user = User.find_by(netid: session[:cas_user])
+    @trips = @user.trips
   end 
 
   def edit
