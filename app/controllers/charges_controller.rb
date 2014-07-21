439757c57eca4e314d4be7dee23fc492d01a654c
Ben Burke <benjamin.burke@yale.edu>
2014-07-21 17:16:21 -0400

    working on email to customers

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -28,6 +28,7 @@ class ChargesController < ApplicationController
 
     if charge.paid == true
       flash.now[:success] = "Payment processed! Thanks " + @user.name.split(" ")[0] + ", you should receive a confirmation email shortly."
+      @trip.update_attributes(payment_status: true)
     else
       flash.now[:error] = "Payment failed, please try again"
     end 
